dc50d9f537cc5ca75dbd721ffcb5579e46937211
Nikita <kavanzus.pochta@gmail.com>
2022-11-11 09:16:24 +0100

    Added: header responsiveness

--- js/script.js
+++ js/script.js
@@ -15,17 +15,24 @@ new Swiper('.swiper__content', {
 
 const defaultOffset = 50;
 const header = document.querySelector('.header');
+const headerList = document.querySelector('.header__list');
 
 const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
 
 window.addEventListener('scroll', () => {
-    if(scrollPosition() > defaultOffset) {
-        //scroll down
-        header.style.backgroundColor = '#262626d9';
-        document.querySelector('.header__content').style.padding = '20px 40px';
+    if (!headerList.classList.contains('active')) {
+        if(scrollPosition() > defaultOffset) {
+            //scroll down
+            header.style.backgroundColor = '#262626d9';
+            document.querySelector('.header__content').style.padding = '20px 40px';
+        }
+        else if(scrollPosition() < defaultOffset){
+            //scroll up
+            header.style.backgroundColor = 'transparent';
+            document.querySelector('.header__content').style.padding = '40px';
+        }
     }
-    else if(scrollPosition() < defaultOffset){
-        //scroll up
+    else {
         header.style.backgroundColor = 'transparent';
         document.querySelector('.header__content').style.padding = '40px';
     }
@@ -42,3 +49,20 @@ for (let i = 0; i < menuTabs.length; i++) {
         menuBodies[i].classList.add('active');
     });
 }
+
+const menuBtn = document.querySelector('.header__menu__btn');
+const body = document.body;
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('active');
+    headerList.classList.toggle('active');
+    body.classList.toggle('lock');
+
+    if (headerList.classList.contains('active')) {
+        header.style.backgroundColor = 'transparent';
+        document.querySelector('.header__content').style.padding = '40px';
+    } else if (!headerList.classList.contains('active') && scrollPosition() > defaultOffset) {
+        header.style.backgroundColor = '#262626d9';
+        document.querySelector('.header__content').style.padding = '20px 40px';
+    }
+});
